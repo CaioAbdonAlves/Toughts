0364aa6b9e27030d7217c916fa07c5c7260568f0
Caio Abdon <caio.alves54@etec.sp.gov.br>
2023-09-14 23:42:16 -0300

    feat: criação das rotas de toughts

---
+++ routes/toughtsRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const router = express.Router();
+const ToughtController = require('../controllers/ToughtController.js');
+
+//controller
+
+router.get('/', ToughtController.showToughts);
+
+module.exports = router;
